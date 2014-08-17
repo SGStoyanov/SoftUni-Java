3813a622d54d332cb3908fc3b6f5be1f21f67c2f
Stoyanov <stoyanov.vn@gmail.com>
2014-08-17 14:03:25 +0300

    Repo reorganized

--- 1.Intro-Java-Homework/run.sh
+++
@@ -1,1 +0,0 @@
-java -jar DeckOfCardsRunnable.jar itextpdf-5.5.0.jar main.Main
--- 2.Java-Syntax-Homework/src/syntaxPackage/TestTest.java
+++
@@ -1,9 +0,0 @@
-package syntaxPackage;
-
-public class TestTest {
-
-	public static void main (String[] args) {
-		
-		System.out.println("Helllooooooooooooo");
-	}
-}
