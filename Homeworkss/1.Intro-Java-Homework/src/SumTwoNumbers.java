64be36c90e949b530ed813783ad093c508a3ad3d
Stoyanov <stoyanov.vn@gmail.com>
2014-08-17 14:10:32 +0300

    Repo reorganized

---
+++ Homeworkss/1.Intro-Java-Homework/src/SumTwoNumbers.java
@@ -0,0 +1,17 @@
+//package introPackage;
+
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int intA = scan.nextInt();
+		int intB = scan.nextInt();
+		scan.close();
+		
+		System.out.println("result: " + (intA + intB));
+	}
+
+}
